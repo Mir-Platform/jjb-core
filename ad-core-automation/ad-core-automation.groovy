3620761b9d8a07de2032b38e80488b0f32ba8f52
Igor Nikolaev <nightvich@gmail.com>
2020-09-18 10:49:52 +0300

    add ad-core-automation example

---
+++ ad-core-automation/ad-core-automation.groovy
@@ -0,0 +1,47 @@
+
+def meta_location = params.META_LOCATION
+def git_cred_id = params.GIT_CRED_ID
+def git_branch = params.GIT_BRANCH
+
+pipeline {
+    agent {
+        label 'common-centos'
+    }
+
+    stages {
+        stage('Clone git repos') {
+            steps {
+                echo 'Clone meta'
+                dir('meta') {
+                    git credentialsId: "${git_cred_id}",
+                    url: 'git@github.com:Mir-Platform/meta-example.git'
+                }
+                echo 'Clone ad-core-automation'
+                dir('auto') {
+                    git credentialsId: "${git_cred_id}",
+                    branch: git_branch,
+                    url: 'git@github.com:Mir-Platform/ad-core-automation.git'
+                }
+            }
+        }
+
+        stage('Install and run') {
+            steps {
+                echo 'Install requirements'
+
+                withDockerContainer('python:3.8.2-slim') {
+                    withEnv(["HOME=${env.WORKSPACE}"]) {
+                    sh 'pip install --user --upgrade -r auto/requirements.txt'
+                    echo 'Run automation'
+                    withCredentials([usernamePassword(credentialsId: 'ad_tech', passwordVariable: 'ad_pass', usernameVariable: 'ad_user')]) {
+                        dir('auto') {
+                                sh "./run.py -u $ad_user -p $ad_pass -f ../meta/${meta_location}"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
